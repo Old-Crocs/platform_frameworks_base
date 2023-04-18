890ade363980c77903088eaf34957f2af3d5c504
Mr_ZerO <rbesar210311@gmail.com>
2023-04-18 17:48:26 +0000

    SettingLib: Add deep sleep info to uptime preference Add deep sleep ratio to "About Phone" -> "Uptime" original from Stefan
    **DU Edits**
    - Simplify the code used to show Deep sleep
    
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/deviceinfo/AbstractUptimePreferenceController.java
+++ packages/SettingsLib/src/com/android/settingslib/deviceinfo/AbstractUptimePreferenceController.java
@@ -25,6 +25,7 @@ import android.text.format.DateUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
+import com.android.settingslib.R;
 
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -89,7 +90,14 @@ public abstract class AbstractUptimePreferenceController extends AbstractPrefere
     }
 
     private void updateTimes() {
-        mUptime.setSummary(DateUtils.formatElapsedTime(SystemClock.elapsedRealtime() / 1000));
+        float deepSleepRatio = Math.max((float) (SystemClock.elapsedRealtime() - SystemClock.uptimeMillis()), 0f)
+                / SystemClock.elapsedRealtime();
+        int deepSleepPercent = Math.round(deepSleepRatio * 100);
+
+        String summary = DateUtils.formatElapsedTime(SystemClock.elapsedRealtime() / 1000) +
+                " " + mContext.getString(R.string.status_deep_sleep, deepSleepPercent, "%");
+
+        mUptime.setSummary(summary.toString());
     }
 
     private static class MyHandler extends Handler {
