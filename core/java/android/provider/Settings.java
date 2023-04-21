4f5695ad183db2afeb5ca149dc2941e7956d8ccf
romeo <rbesar210311@gmail.com>
2023-04-21 11:07:44 +0700

    Fix class Global not found
    Signed-off-by: romeo <rbesar210311@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17477,14 +17477,14 @@ public final class Settings {
          * @hide
          */
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
-    }
 
-    /**
+        /**
          * Android P animation style
          * @hide
          */
         @Readable
         public static final String PIE_ANIMATION_STYLE = "pie_animation_style";
+ }
 
     /**
      * Configuration system settings, containing settings which are applied identically for all
