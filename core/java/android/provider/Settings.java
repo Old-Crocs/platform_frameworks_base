ab3e8dd0f7687c12de73630c45f6865c731712c0
Mr_ZerO <rbesar210311@gmail.com>
2023-06-01 12:25:59 +0000

    SystemUI: Add QS Page Custom Transition Animations [1/2]
    *Mr_ZerO: Adapt To A12L
    
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6216,6 +6216,12 @@ public final class Settings {
         public static final String MAX_VISIBLE_NOTIFICATION_ICONS = "max_visible_notification_icons";
 
         /**
+         * Whether to settings custom transitions PageTileLayout
+         * @hide
+         */
+        public static final String CUSTOM_TRANSITIONS_KEY = "custom_transitions_page_tile";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
