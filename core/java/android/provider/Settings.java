11be714d7382b5ca8e38d8b46302ebc5deb240d7
Mr- 0 <oldcrocs21@gmail.com>
2023-06-17 11:44:18 +0000

    Revert "SystemUI: Introduce Adaptive Playback [1/2]"
    This reverts commit f37c94bcf11f35581907e7340d7bff3c1525a478.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6084,22 +6084,6 @@ public final class Settings {
         public static final String FOD_NIGHT_LIGHT = "fod_night_light";
 
         /**
-         * Adaptive playback
-         * Automatically pause media when the volume is muted and
-         * will resume automatically when volume is restored.
-         *   0 = disabled
-         *   1 = enabled
-         * @hide
-         */
-        public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
-
-        /**
-         * Adaptive playback's timeout in ms
-         * @hide
-         */
-        public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
-
-        /**
          * reTicker Status
          * @hide
          */
